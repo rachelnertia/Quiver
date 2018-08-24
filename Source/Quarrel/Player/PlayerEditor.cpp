4af1460de44a006c2f4bc2d9c1564a19af522793
Rachel <nershly@gmail.com>
2018-08-24 19:35:15 +0100

    Update PlayerEditor
    Add more controls over movement speed

--- Source/Quarrel/Player/PlayerEditor.cpp
+++ Source/Quarrel/Player/PlayerEditor.cpp
@@ -7,16 +7,40 @@ void ImGuiControls(DamageCount& damageCounter, const int damageMaximumLimit) {
 	ImGui::SliderInt("Damage Maximum", &damageCounter.max,    0, damageMaximumLimit);
 }
 
-void PlayerEditor::GuiControls() {
-	ImGui::SliderFloat(
-		"Move Speed", 
-		Target().mMoveSpeed, 
-		&MovementSpeed::GetBase, 
-		&MovementSpeed::SetBase, 
-		0.0f, 
-		20.0f);
+void ImGuiControls(
+	MovementSpeed& movementSpeed, 
+	const float baseLimit,
+	const float multiplierLimit) 
+{
+	ImGui::Text("Move Speed: %.2f", movementSpeed.Get());
 
-	if (ImGui::CollapsingHeader("Damage##PlayerDamage")) {
+	ImGui::SliderFloat(
+		"Move Speed Base",
+		movementSpeed,
+		&MovementSpeed::GetBase,
+		&MovementSpeed::SetBase,
+		0.0f,
+		baseLimit);
+
+	ImGui::SliderFloat(
+		"Move Speed Multiplier",
+		movementSpeed,
+		&MovementSpeed::GetMultiplier,
+		&MovementSpeed::SetMultiplier,
+		0.0f,
+		multiplierLimit);
+	
+	if (ImGui::Button("Reset Multiplier")) {
+		movementSpeed.ResetMultiplier();
+	}
+}
+
+void PlayerEditor::GuiControls() {
+	if (ImGui::CollapsingHeader("Movement##PlayerEditor")) {
+		ImGuiControls(Target().mMoveSpeed, 20.0f, 3.0f);
+	}
+
+	if (ImGui::CollapsingHeader("Damage##PlayerEditor")) {
 		ImGui::Checkbox("Cannot Die", &Target().mCannotDie);
 		ImGuiControls(Target().mDamage, 100);
 	}
